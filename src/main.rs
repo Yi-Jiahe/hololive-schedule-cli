a2b47fcb36f46000f101117e0ee04285c325f634
yijiahe <yi.jiahe00@gmail.com>
2022-06-27 07:06:13 +0000

    Parse response with scraper crate

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 fn main() {
     let args = Cli::parse();
 
-    let response = reqwest::blocking::get(
-        "https://schedule.hololive.tv/simple",
-    )
-    .unwrap()
-    .text()
-    .unwrap();
-    dbg!(response);
+    let response = reqwest::blocking::get("https://schedule.hololive.tv/simple").unwrap();
+
+    let body = response.text().unwrap();
+
+    let document = scraper::Html::parse_document(&body);
+
+    let content_selector = scraper::Selector::parse(".tab-content").unwrap();
+    let link_selector = scraper::Selector::parse("a").unwrap();
+
+    let content = document.select(&content_selector).next().unwrap();
+    dbg!(content.value());
+
+    for element in content.select(&link_selector) {
+        if let Some(youtube_link) = element.value().attr("href") {
+            let parsed_string = element.text().collect::<Vec<_>>()[0]
+                .split(&['\n', ' '][..])
+                .filter(|x| !x.is_empty())
+                .collect::<Vec<_>>();
+            let icon = parsed_string.get(2);
+            println!("{} {:20} {}", parsed_string[0], parsed_string[1], youtube_link);
+        }
+    }
 }
 
 use clap::Parser;
@@ -16,7 +31,6 @@ use clap::Parser;
 #[derive(Parser)]
 struct Cli {
     /// The pattern to look for
-    #[clap(default_value="", short = 'p', long = "pattern")]
+    #[clap(default_value = "", short = 'p', long = "pattern")]
     pattern: String,
 }
-
