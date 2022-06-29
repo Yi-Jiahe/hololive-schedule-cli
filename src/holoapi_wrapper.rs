efa94b9046256711959cc3e9d6383e231924519b
yijiahe <yi.jiahe00@gmail.com>
2022-06-29 03:05:16 +0000

    Add channel struct and println

--- src/holoapi_wrapper.rs
+++ src/holoapi_wrapper.rs
@@ -3,6 +3,18 @@ use serde::Deserialize;
 const HOST: &str = "https://api.holotools.app/v1";
 
 #[derive(Deserialize, Debug)]
+struct Channel {
+    id: i32,
+    yt_channel_id: Option<String>,
+    bb_space_id: Option<String>,
+    name: String,
+    description: Option<String>,
+    photo: Option<String>,
+    published_at: String,
+    twitter_link: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
 struct LiveVideo {
     id: i32,
     yt_video_key: Option<String>,
@@ -13,6 +25,7 @@ struct LiveVideo {
     live_start: Option<String>,
     live_end: Option<String>,
     live_viewers: Option<i32>,
+    channel: Channel,
 }
 
 #[derive(Deserialize, Debug)]
@@ -24,6 +37,35 @@ struct LiveResponse {
 }
 
 pub fn get_live() {
-    let response = dbg!(reqwest::blocking::get(format!("{}{}", HOST, "/live")).unwrap());
-    let deserialized_response = dbg!(response.json::<LiveResponse>());
+    let response = reqwest::blocking::get(format!("{}{}?{}", HOST, "/live", "hide_channel_desc=1")).unwrap();
+    let deserialized_response = response.json::<LiveResponse>();
+    match deserialized_response {
+        Result::Ok(LiveResponse { live, upcoming, ended, .. }) => {
+            println!("--- Live ---");
+            for video in live {
+                if let Some(start) = video.live_start {
+                    println!("{} {} {}", start, video.channel.name, video.title)
+                }
+            }
+            println!("");
+
+            println!("--- Upcoming ---");
+            for video in upcoming {
+                if let Some(start) = video.live_schedule {
+                    println!("{} {} {}", start, video.channel.name, video.title)
+                }
+            }
+            println!("");
+
+            println!("--- Ended ---");
+            for video in ended {
+                if let Some(start) = video.live_start {
+                    println!("{} {} {}", start, video.channel.name, video.title)
+                }
+            }
+            println!("");
+        }
+        Result::Err(Error) => println!("Error")
+    }
+
 }
