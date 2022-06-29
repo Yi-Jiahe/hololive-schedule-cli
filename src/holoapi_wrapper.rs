a49a054c984aac6d734254a0cf55a62b862239ae
yijiahe <yi.jiahe00@gmail.com>
2022-06-29 03:45:49 +0000

    Add error handling for invalid parameters

--- src/holoapi_wrapper.rs
+++ src/holoapi_wrapper.rs
@@ -30,42 +30,50 @@ struct LiveVideo {
 
 #[derive(Deserialize, Debug)]
 struct LiveResponse {
-    live: Vec<LiveVideo>,
-    upcoming: Vec<LiveVideo>,
-    ended: Vec<LiveVideo>,
-    cached: bool,
+    live: Option<Vec<LiveVideo>>,
+    upcoming: Option<Vec<LiveVideo>>,
+    ended: Option<Vec<LiveVideo>>,
+    cached: Option<bool>,
+    message: Option<String>
 }
 
 pub fn get_live() {
-    let response = reqwest::blocking::get(format!("{}{}?{}", HOST, "/live", "hide_channel_desc=1")).unwrap();
+    let response = reqwest::blocking::get(format!("{}{}?{}", HOST, "/live", "hide_channel_desc=1&max_upcoming_hours=48&lookback_hours=11")).unwrap();
     let deserialized_response = response.json::<LiveResponse>();
     match deserialized_response {
-        Result::Ok(LiveResponse { live, upcoming, ended, .. }) => {
-            println!("--- Live ---");
-            for video in live {
-                if let Some(start) = video.live_start {
-                    println!("{} {} {}", start, video.channel.name, video.title)
+        Result::Ok(LiveResponse { live, upcoming, ended, message, .. }) => {
+            if let (Some(mut live), Some(mut upcoming), Some(mut ended)) = 
+            (live, upcoming, ended) {
+                    println!("--- Live ---");
+                    live.sort_by(|a, b| a.live_start.cmp(&b.live_start));
+                    for video in live {
+                        if let Some(start) = video.live_start {
+                            println!("{} {} {}", start, video.channel.name, video.title)
+                        }
+                    }
+                    println!("");
+        
+                    println!("--- Upcoming ---");
+                    for video in upcoming {
+                        if let Some(start) = video.live_schedule {
+                            println!("{} {} {}", start, video.channel.name, video.title)
+                        }
+                    }
+                    println!("");
+        
+                    println!("--- Ended ---");
+                    for video in ended {
+                        if let Some(start) = video.live_start {
+                            println!("{} {} {}", start, video.channel.name, video.title)
+                        }
+                    }
+                    println!("");
                 }
+            else if let Some(message) = message {
+                println!("{}", message);
             }
-            println!("");
-
-            println!("--- Upcoming ---");
-            for video in upcoming {
-                if let Some(start) = video.live_schedule {
-                    println!("{} {} {}", start, video.channel.name, video.title)
-                }
-            }
-            println!("");
-
-            println!("--- Ended ---");
-            for video in ended {
-                if let Some(start) = video.live_start {
-                    println!("{} {} {}", start, video.channel.name, video.title)
-                }
-            }
-            println!("");
         }
-        Result::Err(Error) => println!("Error")
+        Result::Err(error) => println!("Error: {}", error)
     }
 
 }
