616d8eef9a6fd4d1c1fc48e47dda5728e006efc0
Yi Jiahe <yi.jiahe00@gmail.com>
2022-09-29 17:28:10 +0800

    add function to trim string w/ Japanese characters to length

--- src/formatter.rs
+++ src/formatter.rs
@@ -24,10 +24,12 @@ pub fn format_line(
     // Format channel name to be the fixed length
     let channel_name_width = UnicodeWidthStr::width(&channel_name[..]);
 
-    let channel_name_col = if config.format.channel_name_col_length >= channel_name_width {
+    let channel_name_col = if config.format.channel_name_col_length > channel_name_width {
         let channel_name_padding =
             " ".repeat(config.format.channel_name_col_length - channel_name_width);
         format!("{}{}", channel_name, channel_name_padding)
+    } else if config.format.channel_name_col_length < channel_name_width {
+        trim_to_length(&channel_name[..], config.format.channel_name_col_length)
     } else {
         channel_name
     };
@@ -43,4 +45,32 @@ pub fn format_line(
         LiveStatus::Live => Red.paint(line).to_string(),
         LiveStatus::Upcoming | LiveStatus::Other => line,
     }
+}
+
+fn trim_to_length(s: &str, desired_width: usize) -> String {
+    let mut left = 0;
+    let mut right = s.len();
+    let mut pivot = right / 2;
+    while !s.is_char_boundary(pivot) {
+        pivot -= 1;
+    }
+
+    while right - 1 > left {
+        let width = UnicodeWidthStr::width(&s[..pivot]);
+        if width < desired_width - 2 {
+            left = pivot + 1;
+        } else if width > desired_width {
+            right = pivot - 1;
+        } else {
+            break;
+        }
+        pivot = (right + left)/2;
+        while !s.is_char_boundary(pivot) {
+            pivot -= 1;
+        }
+    }
+
+    let padding = " ".repeat(desired_width - UnicodeWidthStr::width(&s[..pivot]));
+
+    format!("{}{}", &s[..pivot], padding)
 }
