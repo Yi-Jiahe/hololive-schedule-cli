72bc8576762299734f029bf82dadb123accbc0ed
Yi Jiahe <yi.jiahe00@gmail.com>
2022-09-29 16:02:19 +0800

    move reading and writing config to file to config module

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,7 @@
+use std::fs;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -10,5 +14,27 @@ impl Config {
         Config{
             holodex_api_token: "".to_string()
         }
+    }
+
+    pub fn from_file(path: &str) -> Result<Config, std::io::Error> {
+        let mut config_file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(path)?;
+
+        let mut contents = String::new();
+        config_file.read_to_string(&mut contents)?;
+        let config = toml::from_str(&contents).unwrap();
+        Ok(config)
+    }
+
+    pub fn write_to_file(&self, path: &str) {
+        let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(path).unwrap();
+        file.write_all(toml::to_string(&self).unwrap().as_bytes());
     }
 }
